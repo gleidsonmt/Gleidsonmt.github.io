679369777536e9eca4f4f6d83adce0a0e1d43dfc
Gleidson <gleidisonmt@gmail.com>
2023-03-19 22:54:33 -0300

    updating project to build

--- lang/lang.js
+++ lang/lang.js
@@ -2,19 +2,20 @@ var langs = {'pt' : "Português", 'en' : "English"};
 var langCode = '';
 var langJS = null;
 
+langCode = navigator.language.substr (0, 2);
+ // alert(langCode);
+  langCode = 'en';
+
+if (langCode in langs) {
+	$.getJSON('lang/'+langCode+'.json', translate);
+}
 
 var translate = function (jsdata)
 {
+
 	$("[tkey]").each (function (index)
 	{
 		var strTr = jsdata [$(this).attr ('tkey')];
 	    $(this).html (strTr);
 	});
 }
-
-langCode = navigator.language.substr (0, 2);
-// alert(langCode);
-// langCode = 'en';
-
-if (langCode in langs)
-	$.getJSON('lang/'+langCode+'.json', translate);
