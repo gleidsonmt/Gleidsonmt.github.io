8b1a8769155db29cedbd4fd7fbe4c3e3d7a3856b
Gleidson Neves da Silveira <gleidisonmt@gmail.com>
2024-09-15 14:37:36 -0300

    test with post mark

--- src/pages/blog/Post.jsx
+++ src/pages/blog/Post.jsx
@@ -4,10 +4,8 @@ import Data from "../posts_data";
 import { useLocation, useParams } from "react-router-dom";
 import hljs from "../codeblocks/web/highlight";
 
-import Markdown from "react-markdown";
-import remarkGfm from "remark-gfm";
-
 import "../codeblocks/web/styles/github.min.css";
+import PostMarked from "./PostMarked";
 
 const Post = () => {
   const { id } = useParams();
@@ -25,37 +23,12 @@ const Post = () => {
       // fetch("http://localhost:5173/assets/posts/posts_00.md")
       .then((res) => res.text())
       .then((text) => setContent(text));
+    highCode();
   }, []);
 
-  // fetch(`http://localhost:5000/projects/${project.id}`, {
-  //   method: "PATCH",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify(project),
-  // })
-  //   .then((resp) => resp.json())
-  //   .then((data) => {
-  //     setProject(data);
-  //     setShowProjectForm(false);
-  //     // mensagem
-  //     setMessage("Projeto Atualizado");
-  //     setType("success");
-  //   })
-  //   .catch((error) => console.log(error));
-
-  // useEffect(() => {
-  //   window.addEventListener("load", highCode);
-  //   return () => {
-  //     window.removeEventListener("load", highCode);
-  //   };
-  // });
-
-  // function highCode() {
-  //   hljs.highlightAll();
-  // }
-
-  console.log(content);
+  function highCode() {
+    hljs.highlightAll();
+  }
 
   return (
     // <div className="grid grid-cols-1 p-24 place-items-start">
@@ -71,107 +44,7 @@ const Post = () => {
         </div>
 
         <div className="flex flex-col justify-start items-start w-full max-w-5xl">
-          <Markdown
-            remarkPlugins={[[remarkGfm, { singleTilde: false }]]}
-            className="flex flex-col gap-2 w-full text-left"
-            components={{
-              // // Map `h1` (`# heading`) to use `h2`s.
-              // h1: "h2",
-              // // Rewrite `em`s (`*like so*`) to `i` with a red foreground color.
-              // em(props) {
-              //   const { node, ...rest } = props;
-              //   return <i style={{ color: "red" }} {...rest} />;
-              // },
-              // code: "code",
-
-              // code(props) {
-              //   const { node, className, ...rest } = props;
-              //   console.log(className);
-              //   return (
-              //     // <BlockCode language="css" {...rest}>
-              //     //   node
-              //     // </BlockCode>
-              //     <code
-              //       id="block"
-              //       className="text-left language-css bg-gray-600 w-full"
-              //       {...rest}
-              //     />
-              //   );
-              // },
-
-              h2(props) {
-                console.log("here");
-                const { ...rest } = props;
-                return <h2 className="text-gray-600 text-7xl mt-2" {...rest} />;
-              },
-
-              h3(props) {
-                const { ...rest } = props;
-                return <h2 className="text-gray-600 text-4xl mt-2" {...rest} />;
-              },
-
-              p(props) {
-                const { ...rest } = props;
-                return <p className="text-gray-500 text-3xl mt-2" {...rest} />;
-              },
-
-              img(props) {
-                const { node, ...rest } = props;
-                return <img className="max-w-3xl" {...rest} />;
-              },
-
-              pre(props) {
-                const { node, children, ...rest } = props;
-                let language = "java";
-                if (children.props.className != undefined) {
-                  language = children.props.className;
-                }
-
-                const { ...res } = children.props;
-                return (
-                  <pre className="text-left bg-gray-100 w-full p-4" {...rest}>
-                    <code
-                      id="block"
-                      style={{
-                        backgroundColor:
-                          "rgb(243 244 246 / var(--tw-bg-opacity))",
-                      }}
-                      className={`text-left  w-full bg-gray-100 ${language}`}
-                      {...res}
-                    ></code>
-                  </pre>
-                );
-              },
-
-              // code(props) {
-              //   const { children, className, node, ...rest } = props;
-              //   const match = /language-(\w+)/.exec(className || "");
-              //   return match ? (
-              //     <SyntaxHighlighter
-              //       {...rest}
-              //       PreTag="div"
-              //       children={String(children).replace(/\n$/, "")}
-              //       language={match[1]}
-              //       style={dark}
-              //     />
-              //   ) : (
-              //     <code {...rest} className={className}>
-              //       {children}
-              //     </code>
-              //   );
-              // },
-            }}
-          >
-            {content}
-          </Markdown>
-
-          {/* <BlockCode
-          code={`.button {
-              -fx-background-color: blue;
-          }`}
-          language="css"
-        ></BlockCode>
-        */}
+          <PostMarked content={content} />
         </div>
       </div>
       {hljs.highlightAll()}
---
+++ src/pages/blog/PostMarked.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+const PostMarked = ({ content }) => {
+  return (
+    <Markdown
+      remarkPlugins={[[remarkGfm, { singleTilde: false }]]}
+      className="flex flex-col gap-2 w-full text-left"
+      components={{
+        // // Map `h1` (`# heading`) to use `h2`s.
+        // h1: "h2",
+        // // Rewrite `em`s (`*like so*`) to `i` with a red foreground color.
+        // em(props) {
+        //   const { node, ...rest } = props;
+        //   return <i style={{ color: "red" }} {...rest} />;
+        // },
+        // code: "code",
+
+        // code(props) {
+        //   const { node, className, ...rest } = props;
+        //   console.log(className);
+        //   return (
+        //     // <BlockCode language="css" {...rest}>
+        //     //   node
+        //     // </BlockCode>
+        //     <code
+        //       id="block"
+        //       className="text-left language-css bg-gray-600 w-full"
+        //       {...rest}
+        //     />
+        //   );
+        // },
+
+        h2(props) {
+          console.log("here");
+          const { ...rest } = props;
+          return <h2 className="text-gray-600 text-7xl mt-2" {...rest} />;
+        },
+
+        h3(props) {
+          const { ...rest } = props;
+          return <h2 className="text-gray-600 text-4xl mt-2" {...rest} />;
+        },
+
+        p(props) {
+          const { ...rest } = props;
+          return <p className="text-gray-500 text-3xl mt-2" {...rest} />;
+        },
+
+        img(props) {
+          const { node, ...rest } = props;
+          return <img className="max-w-3xl" {...rest} />;
+        },
+
+        pre(props) {
+          const { node, children, ...rest } = props;
+          let language = "java";
+          if (children.props.className != undefined) {
+            language = children.props.className;
+          }
+
+          const { ...res } = children.props;
+          return (
+            <pre className="text-left bg-gray-100 w-full p-4" {...rest}>
+              <code
+                id="block"
+                style={{
+                  backgroundColor: "rgb(243 244 246 / var(--tw-bg-opacity))",
+                }}
+                className={`text-left  w-full bg-gray-100 ${language}`}
+                {...res}
+              ></code>
+            </pre>
+          );
+        },
+
+        // code(props) {
+        //   const { children, className, node, ...rest } = props;
+        //   const match = /language-(\w+)/.exec(className || "");
+        //   return match ? (
+        //     <SyntaxHighlighter
+        //       {...rest}
+        //       PreTag="div"
+        //       children={String(children).replace(/\n$/, "")}
+        //       language={match[1]}
+        //       style={dark}
+        //     />
+        //   ) : (
+        //     <code {...rest} className={className}>
+        //       {children}
+        //     </code>
+        //   );
+        // },
+      }}
+    >
+      {content}
+    </Markdown>
+  );
+};
+
+export default PostMarked;
