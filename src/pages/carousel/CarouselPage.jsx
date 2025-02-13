ba2a51ee71e33780e4b754d142f7d61b561a2e31
Gleidson Neves da Silveira <gleidisonmt@gmail.com>
2025-02-13 12:23:08 -0300

    update carousel

--- src/pages/carousel/CarouselPage.jsx
+++ src/pages/carousel/CarouselPage.jsx
@@ -10,21 +10,21 @@ import global from "../../Global";
 export const CarouselPage = () => {
   const [slide, setSlide] = useState(0);
 
-  // if (window.location.host.includes("localhost")) {
-  //   global.assets = "";
-  // }
+  if (window.location.host.includes("localhost")) {
+    global.assets = "";
+  }
 
   const nextSlide = () => {
     setSlide(slide == slides.length - 1 ? 0 : slide + 1);
   };
 
   const prevSlide = () => {
-    setSlide(slide === 0 ? slide.length - 1 : slide - 1);
+    setSlide(slide == 0 ? slides.length - 1 : slide - 1);
   };
 
   return (
     <div className="flex flex-col justify-center align-center place-items-center">
-      <div className="carousel  max-h-[900px] max-w-[1200px] my-20 mx-20">
+      <div className="carousel  max-h-[900px] max-w-[1200px] my-20 ">
         <AiOutlineArrowLeft className="arrow arrow-left" onClick={prevSlide} />
         {slides.map((item, index) => {
           return (
@@ -40,7 +40,7 @@ export const CarouselPage = () => {
           className="arrow arrow-right "
           onClick={nextSlide}
         />
-        <span className="indicators p-20">
+        <span className="indicators">
           {slides.map((_, index) => {
             return (
               <button
