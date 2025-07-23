c1301453af175b0150e583442422ffe0d065cd3f
Gleidson Neves da Silveira <gleidisonmt@gmail.com>
2025-07-23 10:36:28 -0300

    add a certificate

--- src/pages/Certificates.js
+++ src/pages/Certificates.js
@@ -15,6 +15,13 @@ const certficates = [
         finishedAt: dayjs("2023-03-17").format("YYYY-MM-DD"),
         img: "certificado_responsive_web_design.png",
     },
+    {
+        name: "Front End Development",
+        from: "freeCodeCamp",
+        link: "https://www.freecodecamp.org/certification/fccdea68468-112f-4c80-8d61-d487830519a3/front-end-development-libraries",
+        img: "certificado_front_end_development.png",
+        finishedAt: dayjs("2025-06-20").format("YYYY-MM-DD"),
+    }
 ];
 
 export default certficates;
